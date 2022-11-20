d25b1f4a0a34e0ccd8d16e9dfe36663bf47a3245
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2022-11-20 20:53:33 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,124 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 
 
-class Cappuccino(QMainWindow):
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(454, 369)
+        Dialog.setMinimumSize(QtCore.QSize(454, 369))
+        Dialog.setMaximumSize(QtCore.QSize(454, 369))
+        self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(190, 0, 251, 331))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.verticalLayout.addWidget(self.lineEdit_7)
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.verticalLayout.addWidget(self.lineEdit_6)
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.verticalLayout.addWidget(self.lineEdit_5)
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.verticalLayout.addWidget(self.lineEdit_4)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.verticalLayout.addWidget(self.lineEdit_3)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout.addWidget(self.lineEdit_2)
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(Dialog)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(90, 10, 101, 311))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.label_7 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_7.setObjectName("label_7")
+        self.verticalLayout_2.addWidget(self.label_7)
+        self.label_6 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_6.setObjectName("label_6")
+        self.verticalLayout_2.addWidget(self.label_6)
+        self.label_5 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_2.addWidget(self.label_5)
+        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout_2.addWidget(self.label_4)
+        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_2.addWidget(self.label_3)
+        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label.setObjectName("label")
+        self.verticalLayout_2.addWidget(self.label)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(350, 330, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label_7.setText(_translate("Dialog", "ИД"))
+        self.label_6.setText(_translate("Dialog", "Сорт"))
+        self.label_5.setText(_translate("Dialog", "Обжарка"))
+        self.label_4.setText(_translate("Dialog", "Растворимость"))
+        self.label_3.setText(_translate("Dialog", "Вкус"))
+        self.label_2.setText(_translate("Dialog", "Цена"))
+        self.label.setText(_translate("Dialog", "Объем"))
+        self.pushButton.setText(_translate("Dialog", "Ок"))
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(997, 368)
+        MainWindow.setMinimumSize(QtCore.QSize(997, 368))
+        MainWindow.setMaximumSize(QtCore.QSize(997, 368))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 1001, 271))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.add_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.add_btn.setGeometry(QtCore.QRect(0, 270, 93, 28))
+        self.add_btn.setObjectName("add_btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 997, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Espresso"))
+        self.add_btn.setText(_translate("MainWindow", "Добавить"))
+
+
+class LatteMacchiato(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        self.setupUi(self)
         self.add_func()
         self.show_info()
 
@@ -30,7 +144,7 @@ class Cappuccino(QMainWindow):
         self.tableWidget.setColumnWidth(3, 200)
         self.tableWidget.setColumnWidth(4, 200)
 
-        con = sqlite3.connect('coffee.sqlite.db')
+        con = sqlite3.connect('data/coffee.sqlite.db')
         cur = con.cursor()
 
         result = cur.execute('''SELECT * FROM CoffeeInfo
@@ -46,10 +160,10 @@ class Cappuccino(QMainWindow):
             self.tableWidget.setItem(n, num, QTableWidgetItem(str(elem)))
 
 
-class Adding_Dialog(QDialog):
+class Adding_Dialog(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setupUi(self)
         self.add_func()
 
     def add_func(self):
@@ -78,7 +192,7 @@ class Adding_Dialog(QDialog):
             msg.about(self, 'Ошибка', 'Неверно заполнена форма')
             return False
 
-        con = sqlite3.connect('coffee.sqlite.db')
+        con = sqlite3.connect('data/coffee.sqlite.db')
         cur = con.cursor()
         cur.execute(f'''INSERT INTO CoffeeInfo (id, type, roast_level, 
         solubility, taste, price, size)
@@ -97,7 +211,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    fg = Cappuccino()
+    fg = LatteMacchiato()
     fg.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
